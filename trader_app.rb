7d374c68657369ce9d75320234d6cdf256556ab0
jason crockett <webpasturestudios@gmail.com>
2024-12-26 23:56:47 +0000

    now works asynchronously with the scout

--- trader_app.rb
+++ trader_app.rb
@@ -2,9 +2,9 @@ require_relative 'winner_loader'
 require_relative 'winner_builder'
 require_relative 'trader'
 
-winner_h = WinnerLoader.new.winner_h
-winner_struct = WinnerBuilder.new(winner_h)
-trader = Trader.new winner_struct
 while true
+  winner_h = WinnerLoader.new.winner_h
+  winner_struct = WinnerBuilder.new(winner_h)
+  trader = Trader.new winner_struct,false
   trader.run
 end
