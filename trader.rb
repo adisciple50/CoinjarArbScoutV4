759d8cb5eafe4065fee7faf102ba24f057681fa2
jason crockett <webpasturestudios@gmail.com>
2024-12-26 23:30:51 +0000

    wip

--- trader.rb
+++ trader.rb
@@ -5,27 +5,65 @@ require 'json'
 class Trader
   include Helpers
   attr_accessor :chain
-  def initialize(chain_to_trade,testing=true)
-    @chain = chain_to_trade
+  def initialize(winner_builder_to_trade,testing=true)
+    @chain = winner_builder_to_trade
     if testing
       @coinbase = PrivateSandbox.new
     else
       @coinbase = PrivateClient.new
     end
   end
-  def wait_until_trade_is_complete(orderid)
-    puts JSON.parse @coinbase.get_order(orderid)
-    wait 1
+  def set_status(status_code)
+     File.new('./status.json','w').write(JSON.unparse({'status':status_code}))
+    # file.close
+  end
+  def get_status
+    status = ''
+    File.open('./status.json') do |file|
+      to_parse = file.read
+      status = JSON.parse(to_parse)
+      # puts status
+      file.close
+    end
+    puts status["status"]
+    status["status"]
+  end
+  def wait_until_trade_is_complete(order)
+    order = @coinbase.get_order(order["oid"])
+    status = order["status"]
+    puts JSON.parse order
+    while status != "filled"
+      order = @coinbase.get_order(order["oid"])
+      puts "order status is\n ===="
+      puts JSON.parse order
+      status = order["status"]
+      wait 1
+    end
   end
   def run
-    start_price = determine_price(@chain.start_trade_direction,@chain.start)
-    middle_price = determine_price(@chain.middle_trade_direction,@chain.middle)
-    ending_price = determine_price(@chain.ending_trade_direction,@chain.ending)
-    @start_oid = @coinbase.place_order @chain.start.id,start_price,@chain.start_trade_direction,@chain.start_amount
-    wait_until_trade_is_complete(@start_oid)
-    @middle_oid = @coinbase.place_order @chain.middle.id,middle_price,@chain.middle_trade_direction,@chain.middle_amount
-    wait_until_trade_is_complete(@middle_oid)
-    @ending_oid = @coinbase.place_order @chain.ending.id,ending_price,@chain.ending_trade_direction,@chain.ending_amount
-    wait_until_trade_is_complete(@ending_oid)
+    begin
+    if get_status != 1 || get_status !=2 || get_status !=3
+      set_status 1
+    end
+    rescue
+      set_status 1
+    end
+    if get_status == 1
+      @start_oid = @coinbase.place_order @chain.start,@chain.start_price,@chain.start_trade_direction,@chain.start_amount
+      puts @start_oid
+      wait_until_trade_is_complete(@start_oid)
+      set_status 2
+    end
+    if get_status == 2
+      @middle_oid = @coinbase.place_order @chain.middle,@chain.middle_price,@chain.middle_trade_direction,@chain.middle_amount
+      puts @middle_oid
+      wait_until_trade_is_complete(@middle_oid)
+      set_status 3
+    end
+    if get_status == 3
+      @ending_oid = @coinbase.place_order @chain.ending,@chain.ending_price,@chain.ending_trade_direction,@chain.ending_amount
+      puts @ending_oid
+      wait_until_trade_is_complete(@ending_oid)
+    end
   end
 end
