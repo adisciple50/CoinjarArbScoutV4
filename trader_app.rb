e0c90495267a711fe5d612e898e6ea87e69636c3
jason crockett <webpasturestudios@gmail.com>
2024-12-27 00:27:04 +0000

    failproofing

--- trader_app.rb
+++ trader_app.rb
@@ -2,9 +2,17 @@ require_relative 'winner_loader'
 require_relative 'winner_builder'
 require_relative 'trader'
 
-while true
+def trade
   winner_h = WinnerLoader.new.winner_h
   winner_struct = WinnerBuilder.new(winner_h)
   trader = Trader.new winner_struct,false
   trader.run
+end
+
+while true
+  begin
+    trade
+  rescue
+    trade
+  end
 end
